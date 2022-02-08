39473a375c59eeb24b76ba5cadd71d72fa9e2478
Sergey <enzzomatrix@gmail.com>
2022-02-08 12:28:11 +1100

    upd

--- ESSENTIALS/ESSENTIALS/main.cpp
+++ ESSENTIALS/ESSENTIALS/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <vector>
 
 #include <SFML/Graphics.hpp>
 
@@ -15,64 +16,66 @@ int main() {
 
 	const sf::Vector2u &w_size = window.getSize();
 	sf::View view(sf::FloatRect(0, 0, w_size.x, w_size.y));
-	window.setView(view);
+	view.setCenter(sf::Vector2f(0, 0));
 
-	sf::Vector2i spriteSize(32, 32);
-	sf::Sprite sprite;
-	Animator animator(sprite);
+	std::vector<sf::View> view_list(4, sf::View(sf::FloatRect(0, 0, w_size.x, w_size.y)));
 
-	//Create an animation and get the reference to it
-	auto& idleAnimation = animator.CreateAnimation("Idle", "sprites\\gem_blue.png", sf::seconds(1), true);
-	idleAnimation.AddFrames(sf::Vector2i(0, 0), spriteSize, 8);
+	for (sf::View& v : view_list) {
+		v.setCenter(sf::Vector2f(0, 0));
+	}
+
+	view_list[0].setViewport(sf::FloatRect(0, 0, 0.5f, 0.5f));
+	view_list[1].setViewport(sf::FloatRect(0.5f, 0, 0.5f, 0.5f));
+	view_list[2].setViewport(sf::FloatRect(0.5f, 0.5f, 0.5f, 0.5f));
+	view_list[3].setViewport(sf::FloatRect(0, 0.5f, 0.5f, 0.5f));
+
+	view.setViewport(sf::FloatRect(0, 0, 0.5f, 0.5f));
+
+	sf::Vector2f spriteSize(32, 32);
+	auto texture = AssetManager::GetTexture("sprites\\gem_blue.png");
 	
-	auto& idleAnimationShort = animator.CreateAnimation("IdleShort", "sprites\\gem_blue.png", sf::seconds(.5f), true);
-	idleAnimationShort.AddFrames(sf::Vector2i(0, 0), spriteSize, 8);
+	sf::Sprite sprite1(texture);
+	sprite1.setOrigin(spriteSize * 0.5f);
+	sprite1.setPosition(sf::Vector2f(-80, -80));
+	sprite1.setTextureRect(sf::IntRect(0, 0, 32, 32));
 
-	auto& idleAnimationSmall = animator.CreateAnimation("IdleSmall", "sprites\\gem_blue.png", sf::seconds(1.5f), true);
-	idleAnimationSmall.AddFrames(sf::Vector2i(32, 0), spriteSize, 3);
-	idleAnimationSmall.AddFrames(sf::Vector2i(64, 0), spriteSize, 2);
+	sf::Sprite sprite2(texture);
+	sprite2.setOrigin(spriteSize * 0.5f);
+	sprite2.setPosition(sf::Vector2f(80, -80));
+	sprite2.setTextureRect(sf::IntRect(0, 0, 32, 32));
 
-	auto& idleAnimationOnce = animator.CreateAnimation("IdleOnce", "sprites\\gem_blue.png", sf::seconds(.5f), false);
-	idleAnimationOnce.AddFrames(sf::Vector2i(0, 0), spriteSize, 8);
+	sf::Sprite sprite3(texture);
+	sprite3.setOrigin(spriteSize * .5f);
+	sprite3.setPosition(sf::Vector2f(80, 80));
+	sprite3.setTextureRect(sf::IntRect(0, 0, 32, 32));
+
+	sf::Sprite sprite4(texture);
+	sprite4.setOrigin(spriteSize * .5f);
+	sprite4.setPosition(sf::Vector2f(-80, 80));
+	sprite4.setTextureRect(sf::IntRect(0, 0, 32, 32));
 
 	sf::Clock clock;
 	while (window.isOpen()) {
-		
-		sf::Time deltaTime = clock.restart();
-		animator.Update(deltaTime);
-
 		sf::Event event;
+		
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::KeyPressed) {
 				if (event.key.code == sf::Keyboard::Escape) {
 					window.close();
 				}
-
-				if (event.key.code == sf::Keyboard::Key::A) {
-					animator.SwitchAnimation("Idle");
-					std::cerr.flush();
-					std::cerr << "\rIdle";
-				}
-				else if (event.key.code == sf::Keyboard::Key::S) {
-					animator.SwitchAnimation("IdleShort");
-					std::cerr.flush();
-					std::cerr << "\rIdleShort";
-				}
-				else if (event.key.code == sf::Keyboard::Key::D) {
-					animator.SwitchAnimation("IdleSmall");
-					std::cerr.flush();
-					std::cerr << "\rIdleSmall";
-				}
-				else if (event.key.code == sf::Keyboard::Key::F) {
-					animator.SwitchAnimation("IdleOnce");
-					std::cerr.flush(); 
-					std::cerr << "\rIdleOnce";
-				}
 			}
 		}
 
-		window.clear();
-		window.draw(sprite);
+		window.clear();		
+		
+		for (sf::View& v : view_list) {
+			window.setView(v);
+			window.draw(sprite1);
+			window.draw(sprite2);
+			window.draw(sprite3);
+			window.draw(sprite4);
+		}
+
 		window.display();
 	}
 
