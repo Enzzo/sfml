0712423437a94f9fa342b44b01fe663eb521f692
Sergey <enzzomatrix@gmail.com>
2022-02-11 14:31:59 +1100

    upd

--- 02_essentials/02_essentials/game.cpp
+++ 02_essentials/02_essentials/game.cpp
@@ -10,6 +10,8 @@ Game::Game() :window_(sf::VideoMode(640, 480), "SFML Application")
 	textures_.Load(Textures::ID::Eagle, "Media\\Textures\\Eagle.png");
 
 	player_.setTexture(textures_.Get(Textures::ID::Eagle));
+	sf::FloatRect bounds = player_.getLocalBounds();
+	player_.setOrigin(bounds.width / 2.f, bounds.height / 2.f);
 	player_.setPosition(100.f, 100.f);	
 }
 
@@ -57,11 +59,11 @@ void Game::Update(const sf::Time deltaTime) {
 
 	if (is_moving_up && player_.getPosition().y > 0)
 		movement.y -= player_speed_;
-	if (is_moving_down && player_.getPosition().y < window_.getSize().y - player_.getGlobalBounds().height)
+	if (is_moving_down && player_.getPosition().y < window_.getSize().y)
 		movement.y += player_speed_;
 	if (is_moving_left && player_.getPosition(). x > 0)
 		movement.x -= player_speed_;
-	if (is_moving_right && player_.getPosition().x < window_.getSize().x - player_.getGlobalBounds().width)
+	if (is_moving_right && player_.getPosition().x < window_.getSize().x)
 		movement.x += player_speed_;
 
 	player_.move(movement * deltaTime.asSeconds());
